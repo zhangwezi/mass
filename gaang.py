3d4b6f73a92cbf9216df9b1b3bfd7f4c30dda722
zhangwezi <143313425+zhangwezi@users.noreply.github.com>
2023-08-27 14:43:26 +0700

    Update gaang.py

--- gaang.py
+++ gaang.py
@@ -1,25 +1,23 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+#@title ***SELENIUM SOCKS5***
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+options = {
+        'proxy': {
+            #'http': 'socks5://gtuyuckd-rotate:rf9rgi07oj74@p.webshare.io:80',
+            #'https': 'socks5://gtuyuckd-rotate:rf9rgi07oj74@p.webshare.io:80',
+            'no_proxy': 'localhost,127.0.0.1'
+        }
+    }
 
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
 
-driver.get('http://nytimes.com')
+driver = webdriver.Chrome(options=chrome_options,seleniumwire_options=options)
+#driver.get("http://httpbin.org/ip")
+#print(driver.find_element(By.TAG_NAME, "body").text)
+driver.get("https://gmail.com")
 print(driver.title)
+sleep(99999)
